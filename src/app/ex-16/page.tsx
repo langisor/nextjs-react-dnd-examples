22a6f61a347096f00e768a6c3d67ddcebc023961
farisubuntu <farisubuntu@gmail.com>
2025-06-18 23:23:10 +0300

    update...

--- src/app/ex-16/page.tsx
+++ src/app/ex-16/page.tsx
@@ -1,19 +1,20 @@
 "use client";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { Container } from "./_components/container";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
-export default function Ex17Page() {
+
+export default function Ex16Page() {
   return (
     <div className="flex flex-col items-center mx-auto p-3">
       <DndProvider backend={HTML5Backend}>
         <Container />
         <ExampleNote />
-        
       </DndProvider>
     </div>
   );
 }
+
 function ExampleNote() {
   return (
     <Card className="w-full max-w-2xl mx-auto my-4 p-4 text-sm text-muted-foreground">
@@ -21,18 +22,9 @@ function ExampleNote() {
         Notes about This example:
       </CardTitle>
       <CardContent className="text-lg style-text-muted-foreground">
-        Drag the boxes below and drop them into the dustbin. Note that it has a
-        neutral, an active and a hovered state. The dragged item itself changes
-        opacity while dragged.{" "}
+It demonstrates how a single drop target may accept multiple types, and how those types may be derived from props. It also demonstrates the handling of native files and URLs (try dropping them onto the last two dustbins).
       </CardContent>
     </Card>
   );
 }
-
-/*
-/**
- * Notes about This example:
- * 
- * Drag the boxes below and drop them into the dustbin. Note that it has a neutral, an active and a hovered state. The dragged item itself changes opacity while dragged.
- * 
-*/
+ 
