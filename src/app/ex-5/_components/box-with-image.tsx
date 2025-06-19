3f6cf7d3e1f5ba9320340bf72e8b539fbcefe875
farisubuntu <farisubuntu@gmail.com>
2025-06-19 05:53:36 +0300

    update...

--- src/app/ex-5/_components/box-with-image.tsx
+++ src/app/ex-5/_components/box-with-image.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import type { CSSProperties, FC } from 'react'
-import { DragPreviewImage, useDrag } from 'react-dnd'
+import type { CSSProperties, FC } from "react";
+import { DragPreviewImage, useDrag } from "react-dnd";
 
-import { boxImage } from './box-image'
-import { ItemTypes } from './ItemTypes'
+import { boxImage } from "./box-image";
+import { ItemTypes } from "./ItemTypes";
 
 const style: CSSProperties = {
-  border: '1px dashed gray',
-  padding: '0.5rem 1rem',
-  marginBottom: '.5rem',
-  backgroundColor: 'white',
-  cursor: 'move',
-  width: '20rem',
-}
+  border: "1px dashed gray",
+  padding: "0.5rem 1rem",
+  marginBottom: ".5rem",
+  backgroundColor: "white",
+  cursor: "move",
+  width: "20rem",
+};
 
 export const BoxWithImage: FC = () => {
   const [{ opacity }, drag, preview] = useDrag(() => ({
@@ -21,7 +21,7 @@ export const BoxWithImage: FC = () => {
     collect: (monitor) => ({
       opacity: monitor.isDragging() ? 0.4 : 1,
     }),
-  }))
+  }));
 
   return (
     <>
@@ -30,5 +30,5 @@ export const BoxWithImage: FC = () => {
         Drag me to see an image
       </div>
     </>
-  )
-}
+  );
+};
